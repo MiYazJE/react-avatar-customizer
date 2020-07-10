5882b7b9a051b601e882b47014038415e532acd5
MiYazJE <ruben.saizserrano@gmail.com>
2020-07-10 03:25:41 +0200

    styles

--- src/components/AvatarCustomizer/AvatarCustomizer.jsx
+++ src/components/AvatarCustomizer/AvatarCustomizer.jsx
@@ -94,7 +94,7 @@ const AvatarCustomizer = forwardRef(({ avatar, hideOptions, onChange }, ref) => 
             <div className="avatar">
                 <Avatar
                     style={{ width: '100%', height: '100%' }}
-                    avatarStyle={avatarSelectors['avatarStyle']}
+                    avatarStyle="Circle"
                     topType={avatarSelectors['topType']}
                     accessoriesType={avatarSelectors['accessoriesType']}
                     hairColor={avatarSelectors['hairColor']}
